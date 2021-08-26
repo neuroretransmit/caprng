df9ad11b6f638cb3c57ccf9a2897036721e525f3
John Holly <j@optimistic.ninja>
2021-08-25 21:40:14 -0700

    Cleanup

--- caprng.cc
+++ caprng.cc
@@ -4,25 +4,31 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 typedef uint8_t u8;
 
-const bool LINEAR_ADDITIVE_RULE = false;
-const bool NONLINEAR_ADDITIVE_RULE = true;
-// M = linearity
-const bool linearity = NONLINEAR_ADDITIVE_RULE;
-
+static const bool LINEAR_ADDITIVE_RULE = false;
+static const bool NONLINEAR_ADDITIVE_RULE = true;
+// linearity = M in paper
+static const bool linearity = NONLINEAR_ADDITIVE_RULE;
 // Indices for impact coefficients that match the
 // variable names in the paper
 enum { P, Q, R, S, T };
-template<typename T>
-ostream& operator<< (ostream& out, const vector<T>& v) {
+static vector<vector<bool>> permutation_bases = {
+		{ 0, 0, 0, 0, 1 },
+		{ 0, 0, 0, 1, 1 },
+		{ 0, 0, 1, 1, 1 },
+		{ 0, 1, 1, 1, 1 }
+};
+
+template<typename T> ostream& operator<< (ostream& out, const vector<T>& v) {
 	out << "{";
 	size_t last = v.size() - 1;
 	for(size_t i = 0; i < v.size(); ++i) {
-		out << v[i];
+		out << std::hex << (int) v[i];
 		if (i != last) 
 			out << ", ";
 	}
@@ -34,13 +40,8 @@ ostream& operator<< (ostream& out, const vector<T>& v) {
  * additive rule */
 vector<bitset<5>> enumerate_impact_coefficients()
 {
-	vector<vector<bool>> bases = {
-		{ 0, 0, 0, 0, 1 },
-		{ 0, 0, 0, 1, 1 },
-		{ 0, 0, 1, 1, 1 },
-		{ 0, 1, 1, 1, 1 }};
 	vector<bitset<5>> connectivities;
-	for (auto& base : bases) {
+	for (auto& base : permutation_bases) {
 		do {
 			// Constraint from paper
 			if (base[P] & base[T])
@@ -48,7 +49,6 @@ vector<bitset<5>> enumerate_impact_coefficients()
 			bitset<5> x;
 			for (auto i = 0; i < base.size(); i++)
 				x[i] = base[i];
-			cout << x << endl;
 			connectivities.push_back(x);
 		} while (next_permutation(base.begin(), base.end()));
 	}
@@ -83,13 +83,12 @@ int main()
 	cout << "Class 3 rules: " << c3 << endl;
 	cout << "Class 4 rules: " << c4 << endl;
 
+	vector<u8> random_values;
 	for (auto& rule : class4_rules) {
-
 		for (auto i = 0; i < 25; i++) {
 			// Additive rule CA	
-			cout << "value: " << byte.to_ulong() << endl;
+			random_values.push_back(byte.to_ulong());
 			for (auto i = 0; i < sz; i++) {
-				// TODO: Connectivity, P, Q, R, S, T
 				byte[i] = linearity 
 					^ rule[P] & byte[(i - 2) % sz] 
 					^ rule[Q] & byte[(i - 1) % sz] 
@@ -99,4 +98,5 @@ int main()
 			}
 		}
 	}
+	cout << "Random values: " << random_values << endl;
 }
